c8126143e18ef75409b87f45b822dea157d27505
belhassen07 <chelbi.belhassen@yahoo.fr>
2018-06-29 06:23:52 +0100

    adding more files to cache with the service worker

--- sw.js
+++ sw.js
@@ -1,5 +1,11 @@
 let cacheName = 'cache-v1';
-let urlsToCache = ['/css/index.css', 'css/mdb.min.css', '/exchange.png'];
+let urlsToCache = [
+  '/',
+  '/css/index.css',
+  '/css/mdb.min.css',
+  '/exchange.png',
+  '/index.js',
+];
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
